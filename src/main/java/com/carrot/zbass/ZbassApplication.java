944cb4dc7d167905df324c3963fd5c94e9848d0f
ZBASS - <zbass-@ZBASSui-MacBook-Pro.local>
2022-01-12 23:05:08 +0900

    Initial commit - Project Setting

---
+++ src/main/java/com/carrot/zbass/ZbassApplication.java
@@ -0,0 +1,13 @@
+package com.carrot.zbass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carrot/zbass/ZbassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrot.zbass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
